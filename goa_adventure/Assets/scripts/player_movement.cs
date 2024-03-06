9dc7c48904ab1f15590f8fa6cc1d0aef6c831708
datickhkikvadze <datimine1@gmail.com>
2024-03-06 13:49:46 +0400

    reorebis brdzanebeli

---
+++ goa_adventure/Assets/scripts/player_movement.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    private Rigidbody2D rb;
+    [SerializeField] private AudioSource normal;
+    [SerializeField] private AudioSource sprinting;
+    private Vector2 movementDirection;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprinting.Stop();
+        normal.Stop();
+    }
+
+
+    void Update()
+    {
+        movementDirection = new Vector2(UnityEngine.Input.GetAxis("Horizontal"), UnityEngine.Input.GetAxis("Vertical"));
+        if (movementDirection.magnitude == 5f && !normal.isPlaying)
+        {
+            sprinting.Stop();
+            normal.Play();
+        }
+        else if (movementDirection.magnitude == 0f && normal.isPlaying)
+        {
+            normal.Stop();
+            sprinting.Stop();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movementSpeed * movementDirection;
+
+        if (UnityEngine.Input.GetKey(KeyCode.LeftShift))
+        {
+            sprint();
+            if (movementDirection.magnitude >= 6.99f && !sprinting.isPlaying)
+            {
+                normal.Stop();
+                sprinting.Play();
+            }
+            else if (movementDirection.magnitude < 6.99f && sprinting.isPlaying)
+            {
+                sprinting.Stop();
+            }
+        }
+        else if (UnityEngine.Input.GetKey(KeyCode.LeftControl))
+        {
+            crouch();
+            if (movementDirection.magnitude == 3f && sprinting.isPlaying)
+            {
+
+            }
+        }
+        else
+        {
+            movementSpeed = 5f;
+        }
+    }
+
+    void sprint()
+    {
+        movementSpeed = 7f;
+    }
+    void crouch()
+    {
+        movementSpeed = 3f;
+    }
+}
